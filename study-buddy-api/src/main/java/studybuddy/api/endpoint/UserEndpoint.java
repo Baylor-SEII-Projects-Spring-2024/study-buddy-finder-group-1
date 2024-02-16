a0e9637e088dc0670528a95674e7c942b02c9223
Vincent <vincentt.dinh03@gmail.com>
2024-02-15 23:00:25 -0600

    added change password and delete account functionality

--- study-buddy-api/src/main/java/studybuddy/api/endpoint/UserEndpoint.java
+++ study-buddy-api/src/main/java/studybuddy/api/endpoint/UserEndpoint.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import studybuddy.api.user.User;
 import studybuddy.api.user.UserService;
+import java.util.List;
 
 @Log4j2
 @RestController
@@ -12,6 +13,11 @@ import studybuddy.api.user.UserService;
 public class UserEndpoint {
     @Autowired
     private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> findAllUsers() {
+        return userService.findAllUsers();
+    }
 
     @GetMapping("/users/{id}")
     public User findUserById(@PathVariable Long id) {
@@ -24,8 +30,28 @@ public class UserEndpoint {
         return user;
     }
 
-    @PostMapping("/users")
+    @PostMapping("/register")
     public User saveUser(@RequestBody User user) {
         return userService.saveUser(user);
     }
+
+    @PutMapping("/changePassword/{id}")
+    public void changePassword(@PathVariable Long id, @RequestParam String newPassword) {
+        userService.changePassword(id, newPassword);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteUserById(@PathVariable Long id) {
+        var user = userService.findUser(id).orElse(null);
+
+        if (user == null) {
+            log.warn("User not found");
+            return;
+        }
+
+        userService.deleteUser(id);
+    }
+
+
+
 }
