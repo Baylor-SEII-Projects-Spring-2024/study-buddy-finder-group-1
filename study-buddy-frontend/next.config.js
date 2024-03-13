4b3edf82c2e98f3a9cb6d963ea40da3a21e2be63
nickrevard9 <nickrevard@gmail.com>
2024-03-13 11:37:25 -0500

    Placed register.js in the right file

--- study-buddy-frontend/next.config.js
+++ study-buddy-frontend/next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Only include browser-specific code in the webpack bundle
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        // Enable the usage of 'window' and 'location' in browser environment
+        'assert': false,
+        'fs': false,
+        'path': false,
+        'os': false,
+      };
+    }
+    return config;
+  },
+};
 
 module.exports = nextConfig
