d7d76983bbe4307494ed7d0f821f18b07770040e
Vincent <vincentt.dinh03@gmail.com>
2024-03-15 16:41:20 -0500

    added get friends endpoint

--- study-buddy-api/src/main/java/studybuddy/api/friendships/FriendshipRepository.java
+++ study-buddy-api/src/main/java/studybuddy/api/friendships/FriendshipRepository.java
@@ -9,4 +9,7 @@ import java.util.Optional;
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
     @Query("SELECT f FROM Friendship f WHERE f.status = 'pending'")
     List<Friendship> findAllPending();
+
+    @Query("SELECT f FROM Friendship f WHERE (f.requester.id = :userId OR f.requested.id = :userId) AND f.status = 'accepted'")
+    List<Friendship> findAllByUserIdAndStatusAccepted(Long userId);
 }
