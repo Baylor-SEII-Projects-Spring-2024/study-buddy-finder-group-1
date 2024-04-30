f289cc9930efc185ee2b4c36d6bbe4a0a64d0152
nickrevard9 <54251849+nickrevard9@users.noreply.github.com>
2024-04-30 15:01:33 -0500

    header fix

--- study-buddy-frontend/src/pages/EditClasses.js
+++ study-buddy-frontend/src/pages/EditClasses.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     //baseURL: 'http://localhost:8080', // Replace this with your backend server URL
-    baseURL: 'http://34.125.65.178:8080', // Replace this with your backend server URL
+    baseURL: 'http://34.16.179.242:8080', // Replace this with your backend server URL
 
     timeout: 5000, // Optional: Set a timeout for requests (in milliseconds)
     // Other default configuration options can be added here
